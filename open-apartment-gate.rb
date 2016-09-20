398cd6499413e27cbc55c0efd26c4649c95e34f3
Joe Sortelli <joe@sortelli.com>
2016-09-19 23:59:54 -0700

    Add /open-apartment-gate/ endpoint

--- open-apartment-gate.rb
+++ open-apartment-gate.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
+require 'plivo'
 
-get '/' do
-  'Empty endpoint'
+def open_gate_response_xml()
+  resp = Plivo::Response.new()
+
+  speaking_params = {
+    'language': 'en-US',
+    'voice':    'WOMAN'
+  }
+
+  resp.addDTMF('WWW1WWW', {'async': false})
+
+  resp.addSpeak('Please wait while I automatically open the gate', speaking_params)
+
+  resp.addDTMF('9w9w9w9', {'async': false})
+
+  resp.addSpeak('Goodbye', speaking_params)
+
+  return resp.to_s()
+end
+
+get '/open-apartment-gate/' do
+  content_type 'text/xml'
+  return open_gate_response_xml()
+end
+
+post '/open-apartment-gate/' do
+  content_type 'text/xml'
+  return open_gate_response_xml()
 end
